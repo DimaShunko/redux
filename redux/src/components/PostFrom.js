c9e00efb43546a7ab68a78f11f1ab3947db30bd3
DimaShunko <100087856+DimaShunko@users.noreply.github.com>
2023-02-10 16:17:27 +0300

    Add files via upload

---
+++ redux/src/components/PostFrom.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {connect} from "react-redux";
+import {createPost, showAlert} from "../redux/actions";
+
+class PostFrom extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: ''
+        }
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+
+        const  {title} = this.state
+
+        if(!title.trim()){
+            return this.props.showAlert("Название поста не может быть пустым")
+        }
+
+        const newPost = {
+            title, id: Date.now().toString()
+        }
+
+        this.props.createPost(newPost)
+
+        this.setState({title: ''})
+    }
+
+    changeInputHandler = event => {
+        this.setState(prev => ({...prev, ...{
+           [event.target.name]: event.target.value
+        }}))
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.submitHandler}>
+                {this.props.alert &&
+                    <div className="alert alert-warning" role="alert">
+                        {this.props.alert}
+                    </div>
+                }
+
+                <div className="form-group">
+                    <label className="title">Заголовок поста</label>
+                    <input
+                        type="text"
+                        className='form-control'
+                        id="title"
+                        value={this.state.title}
+                        name="title"
+                        onChange={this.changeInputHandler}
+                    />
+                </div>
+                <button className="btn btn-success" type="submit">Создать</button>
+            </form>
+        )
+    }
+}
+
+const mapDispatchToProps = {
+    createPost, showAlert
+}
+
+const mapStateToProps = state => ({
+    alert: state.app.alert
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostFrom)
